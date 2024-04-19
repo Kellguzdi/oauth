a361dd5fab7f2d47a6270908f5d6219be4a73977
Kellguzdi <20213tn114@utez.edu.mx>
2024-04-18 21:48:50 -0600

    delete about

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,6 @@
       <button v-if="!$auth.isAuthenticated" @click="login">Log in</button>
       <button v-if="$auth.isAuthenticated" @click="logout">Log out</button>
     </div>
-
   </div>
 </template>
 
